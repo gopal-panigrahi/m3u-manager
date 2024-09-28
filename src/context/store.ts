11af3aa335e9809fb1686a8236705cc328b830b5
Gopal Panigrahi <50495221+gopal-panigrahi@users.noreply.github.com>
2024-09-29 01:09:09 +0530

    added store persistence

--- src/context/store.ts
+++ src/context/store.ts
@@ -1,10 +1,40 @@
-import { configureStore } from "@reduxjs/toolkit";
 import playlistReducer from "./playlist.context";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import {
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  persistReducer,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const rootReducer = combineReducers({
+  playlist: playlistReducer,
+});
+
+type RootReducer = ReturnType<typeof rootReducer>;
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer<RootReducer>(
+  persistConfig,
+  rootReducer
+);
 
 export const store = configureStore({
-  reducer: {
-    playlist: playlistReducer,
-  },
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
 export type AppStore = typeof store;
